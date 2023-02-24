8d592729e5a78a23caafd9a2b594af9140803de8
luizcarlossantana <2022211510009@iesp.edu.br>
2023-02-24 19:18:18 -0300

    exemplo

---
+++ Aula 01/aula01.js
@@ -0,0 +1,35 @@
+var aluno = "jao";
+console.log(aluno);
+
+function imprimir(){
+    var aluno02 = "pedro";
+   console.log(aluno02);
+   
+
+   
+}
+
+
+
+if (aluno == "joao"){
+
+    let resultado = "olá joao"
+    console.log(resultado)
+   
+
+} else {
+
+    let resultado ="oi"
+    console.log(resultado)
+}
+
+
+
+
+/*var p =['maria','pedro']
+
+
+for (c of p){
+    console.log(c)
+
+}*/
