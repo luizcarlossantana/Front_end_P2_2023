a5d021851a38a66159914667747831ed171643fc
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2023-03-08 21:16:46 -0300

    ok

---
+++ Aula02/exe.js
@@ -0,0 +1,68 @@
+var lista_altura = [];
+var lista_sexo = [];
+var controle = 0;
+var soma = 0;
+var conta_m = 0;
+var conta_f = 0;
+var media = soma / conta_m;
+
+var res = document.getElementById('res')
+
+
+while (controle < 3) {
+
+    var altura = window.prompt("Digite sua Altura");
+    var sexo = window.prompt("Digite seu Sexo");
+
+    controle++;
+
+
+    lista_altura.push(parseFloat(altura));
+    lista_sexo.push(sexo);
+
+}
+
+
+for (c of lista_altura) {
+   
+    var maior = lista_altura[0];
+    var menor = lista_altura[0];
+
+
+    if (c > maior) {
+
+        maior = c;
+
+    } else if (c < menor) {
+
+        menor = c;
+
+    }
+
+
+
+}
+
+for (d in lista_sexo) {
+
+
+
+    if (lista_sexo[d] == "m") {
+
+        conta_m++;
+
+        soma += lista_altura[d];
+
+
+
+    } else if (lista_sexo[d] == "f") {
+        conta_f++;
+    }
+}
+
+
+res.innerText = `A maior altura do grupo ${maior}`
+
+
+
+
